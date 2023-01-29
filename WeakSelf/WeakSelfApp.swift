7dcd88b485a8205248d94371d21e07848610d74d
Brian McIntosh <brian@mixinteractive.com>
2023-01-29 08:49:33 -0500

    Initial Commit

---
+++ WeakSelf/WeakSelfApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeakSelfApp.swift
+//  WeakSelf
+//
+//  Created by Brian McIntosh on 1/29/23.
+//
+
+import SwiftUI
+
+@main
+struct WeakSelfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
